2a7fa36e7d67289ee108dcc0a1fbf163c03eaa4e
Ubuntu <ubuntu@ip-172-31-5-87.us-east-2.compute.internal>
2020-06-17 20:20:00 +0000

    Secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-
 class User < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :name, presence: true, length: { maximum: 50}
@@ -6,4 +5,6 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255}, 
                       format: { with: VALID_EMAIL_REGEX },
                       uniqueness: true
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 6 }
 end
