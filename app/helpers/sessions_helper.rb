117531e50bddd8f5ed6d51428959117ac204c064
Ubuntu <ubuntu@ip-172-31-5-87.us-east-2.compute.internal>
2020-06-28 18:49:46 +0000

    Login with remembering, added authenticated? method to User model

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,10 +5,20 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  # Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permament.encrypted[:user_id] = user.id
+    cookies.permement[:remember_token] = user.remember_token
+  end
+  
   # Returns the current logged-in user (if any).
   def current_user
-    if session[:user_id]
+    if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+        
     end
   end
   
