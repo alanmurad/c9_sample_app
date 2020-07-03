963633e705ad5ab8421b4b56452bd13fa6909f10
Ubuntu <ubuntu@ip-172-31-5-87.us-east-2.compute.internal>
2020-07-03 02:50:48 +0000

    Succesful edits with TDD

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,10 +9,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   test "unsuccesful edit" do
     get edit_user_path(@user)
     assert_template 'users/edit'
-    patch user_path(@user), params: { users: { name: "",
+    patch user_path(@user), params: { user: { name: "",
                                               email: "foo@invalid",
                                               password: "foo",
                                               password_confirmation: "bar" } }
     assert_template 'users/edit'                                              
+  end
+  
+  test "succesful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name: name,
+                                              email: email,
+                                              password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user 
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
   end
 end
