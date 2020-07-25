9fbc9ddf3275cfb57cd3f1276879485d1eeaad01
Ubuntu <ubuntu@ip-172-31-5-87.us-east-2.compute.internal>
2020-07-25 21:09:14 +0000

    Add user microposts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,9 @@ Rails.application.configure do
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
+  
+  # Store uploaded files on Amazon AWS.
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -125,5 +128,4 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  
 end
